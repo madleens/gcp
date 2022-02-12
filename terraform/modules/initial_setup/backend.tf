0d822cff824598d7c012d5c642c70bd8d4a55636
madleens <madlen-steven@web.de>
2022-02-13 00:56:19 +0100

    add secrets for pipeline

--- terraform/modules/initial_setup/backend.tf
+++ terraform/modules/initial_setup/backend.tf
@@ -11,3 +11,29 @@ resource "random_string" "backend_bucket" {
   number  = false
   upper   = false
 }
+
+resource "google_secret_manager_secret" "backend_config" {
+  project   = var.project_id
+  secret_id = "config_tf"
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "this" {
+  secret      = google_secret_manager_secret.backend_config.id
+  secret_data = "bucket = ${google_storage_bucket.backend.name} \n prefix  =\"terraform/state\""
+  lifecycle {
+    ignore_changes = [
+      secret_data,
+    ]
+  }
+}
+
+resource "google_secret_manager_secret" "terraform_tfvars" {
+  project   = var.project_id
+  secret_id = "terraform_tfvars"
+  replication {
+    automatic = true
+  }
+}
