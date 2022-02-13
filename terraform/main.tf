aebc356003978742fab0da1eb4117932db60d790
madleens <madlen-steven@web.de>
2022-02-13 11:35:44 +0100

    add list for enabled servcies

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
+locals {
+  service_list = [
+    "compute.googleapis.com",
+  ]
+}
 
+resource "google_project_service" "this" {
+  for_each = local.service_list
+
+  project = var.project_id
+  service = each.value
+}
