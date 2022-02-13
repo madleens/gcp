78b687f481709457fad338a3f5445deab2422c60
madleens <madlen-steven@web.de>
2022-02-13 10:56:59 +0100

    let sa access secrets

--- terraform/modules/initial_setup/backend.tf
+++ terraform/modules/initial_setup/backend.tf
@@ -1,3 +1,4 @@
+
 resource "google_storage_bucket" "backend" {
   name                        = random_string.backend_bucket.result
   project                     = var.project_id
@@ -12,7 +13,12 @@ resource "random_string" "backend_bucket" {
   upper   = false
 }
 
-resource "google_secret_manager_secret" "backend_config" {
+resource "google_project_service" "secretmanager" {
+  project = var.project_id
+  service = "secretmanager.googleapis.com"
+}
+
+resource "google_secret_manager_secret" "config_tfvars" {
   project   = var.project_id
   secret_id = "config_tfvars"
   replication {
@@ -20,13 +26,8 @@ resource "google_secret_manager_secret" "backend_config" {
   }
 }
 
-resource "google_project_service" "secretmanager" {
-  project = var.project_id
-  service = "secretmanager.googleapis.com"
-}
-
 resource "google_secret_manager_secret_version" "config" {
-  secret      = google_secret_manager_secret.backend_config.id
+  secret      = google_secret_manager_secret.config_tfvars.id
   secret_data = "bucket = ${google_storage_bucket.backend.name} \n prefix  =\"terraform/state\""
   lifecycle {
     ignore_changes = [
@@ -51,4 +52,22 @@ resource "google_secret_manager_secret_version" "terraform" {
       secret_data,
     ]
   }
+}
+
+resource "google_secret_manager_secret_iam_binding" "config" {
+  project   = google_secret_manager_secret.config_tfvars.project
+  secret_id = google_secret_manager_secret.config_tfvars.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  members = [
+    "serviceAccount:${google_service_account.github_actions.email}",
+  ]
+}
+
+resource "google_secret_manager_secret_iam_binding" "terraform" {
+  project   = google_secret_manager_secret.terraform_tfvars.project
+  secret_id = google_secret_manager_secret.terraform_tfvars.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  members = [
+    "serviceAccount:${google_service_account.github_actions.email}",
+  ]
 }
