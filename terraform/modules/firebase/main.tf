31ccb7f50071b43b8c80db97bce3118172cd01ce
madleens <madlen-steven@web.de>
2022-02-15 22:41:06 +0100

    firbase api has to be enabled

--- terraform/modules/firebase/main.tf
+++ terraform/modules/firebase/main.tf
@@ -1,3 +1,17 @@
+locals {
+  service_list = toset([
+    "firebase.googleapis.com",
+  ])
+}
+
+resource "google_project_service" "this" {
+  for_each = local.service_list
+
+  project            = var.project_id
+  service            = each.value
+  disable_on_destroy = false
+}
+
 resource "google_firebase_project" "default" {
   provider = google-beta
   project  = var.project_id
